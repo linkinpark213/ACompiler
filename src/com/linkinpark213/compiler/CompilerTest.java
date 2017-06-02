6a75f13d70433de4bac4b7baf004bc28eda28afe
linkinpark213 <linkinpark213@outlook.com>
2017-06-02 14:18:03 +0800

    Wish I could do this!

---
+++ src/com/linkinpark213/compiler/CompilerTest.java
@@ -0,0 +1,10 @@
+package com.linkinpark213.compiler;
+
+/**
+ * Created by ooo on 2017/6/2 0002.
+ */
+public class CompilerTest {
+    public static void main(String[] args) {
+        System.out.println("Compiling...");
+    }
+}
---
+++ src/com/linkinpark213/compiler/analyzer/lexical/symbols/Identifier.java
@@ -0,0 +1,7 @@
+package com.linkinpark213.compiler.analyzer.lexical.symbols;
+
+/**
+ * Created by ooo on 2017/6/2 0002.
+ */
+public class Identifier extends Symbol {
+}
